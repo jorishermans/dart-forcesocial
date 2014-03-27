77388b1ad5ba2bea2d4853e627a9bb3b0c1a0794
joris hermans <JHermans85@hotmail.com>
2014-03-27 23:29:31 +0100

    adding more code for the facebook flow

--- lib/social/oauth_controller.dart
+++ lib/social/oauth_controller.dart
@@ -3,12 +3,24 @@ part of dart_social_lib;
 @Controller()
 class OAuthController {
   
+  var appId = "";
+  
   OAuthController() {
-    var doc = loadYaml("YAML: YAML Ain't Markup Language");
+    var doc = loadYaml("clientId: ");
   }
   
   @RequestMapping(value: "/social/facebook/")
-  String variable(req, Model model) {
-      return "redirect:/viewable/";
+  String variable(req, Model model, @RequestParam(value: "redirectUri", defaultValue: "/") redirectUri) {
+      //var  "https://www.facebook.com/dialog/oauth?client_id={app-id}&redirect_uri={redirect-uri}";
+      var redirectUri = "http://localhost:8080/social/facebook/redirected/";
+      var state = "rnd";
+
+      return "redirect:https://www.facebook.com/dialog/oauth?client_id=$appId&redirect-uri=$redirectUri&state=$state";
+  }
+  
+  @RequestMapping(value: "/social/facebook/redirected/")
+  String redirected(req, Model model, @RequestParam(value: "redirectUri", defaultValue: "/") redirectUri) {
+    
+    return "index";
   }
 }
